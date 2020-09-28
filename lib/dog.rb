f5ce3a64511fca314983b281dc28b5f3cef293bf
Carl J <carlj1881@yahoo.com>
2020-09-28 20:14:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,12 +8,12 @@ class Dog
   
   def initialize(name)
     @name = name
-    # @@all << self
-  end
-  
-  def self.save
     @@all << self
   end
+  
+  # def self.save
+  #   @@all << self
+  # end
  
   def self.all
     @@all
